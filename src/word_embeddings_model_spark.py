4f617426c0a7457f2aacd939194839971e577774
Yasser Parambathkandy <yarafatin@gmail.com>
2023-04-11 00:54:00 -0400

    word embeddings spark based

---
+++ src/word_embeddings_model_spark.py
@@ -0,0 +1,55 @@
+import sparknlp
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.linalg import VectorUDT
+from pyspark.ml.linalg import Vectors
+from pyspark.sql.functions import col, udf
+from pyspark.sql.types import ArrayType, DoubleType, IntegerType
+from sparknlp.annotator import *
+from sparknlp.base import *
+
+spark = sparknlp.start()
+data = spark.read.format('csv').option('header', True).load('../data/train.csv').limit(100)
+data = data.withColumn("target", data["target"].cast(IntegerType()))
+data.printSchema()
+
+document_assembler = DocumentAssembler().setInputCol("question_text") \
+    .setOutputCol("document")
+
+tokenizer = Tokenizer().setInputCols(["document"]) \
+    .setOutputCol("token")
+
+embeddings = WordEmbeddingsModel.pretrained("glove_840B_300", "xx") \
+    .setInputCols("document", "token") \
+    .setOutputCol("embeddings")
+
+nlpPipeline = Pipeline(stages=[document_assembler,
+                               tokenizer,
+                               embeddings])
+
+df = nlpPipeline.fit(data).transform(data)
+
+def avg_vectors(bert_vectors):
+  length = len(bert_vectors[0]["embeddings"])
+  avg_vec = [0] * length
+  for vec in bert_vectors:
+    for i, x in enumerate(vec["embeddings"]):
+      avg_vec[i] += x
+    avg_vec[i] = avg_vec[i] / length
+  return avg_vec
+
+
+#create a udf
+avg_vectors_udf = udf(avg_vectors, ArrayType(DoubleType()))
+df_doc_vec = df.withColumn("doc_vector", avg_vectors_udf(col("embeddings")))
+
+def dense_vector(vec):
+    return Vectors.dense(vec)
+
+dense_vector_udf =udf(dense_vector, VectorUDT())
+training = df_doc_vec.withColumn("features", dense_vector_udf(col("doc_vector")))
+
+lr = LogisticRegression(labelCol="target", featuresCol="features", maxIter=10, regParam=0.3, elasticNetParam=0.8)
+lrParisModel = lr.fit(training)
+
+
+
